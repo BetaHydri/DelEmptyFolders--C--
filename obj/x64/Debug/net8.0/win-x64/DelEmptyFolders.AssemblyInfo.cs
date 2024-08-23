992978d768577eec012f6f9bef96644ddd7f077c
BetaHydri <jan_tiedemann@hotmail.com>
2024-08-23 10:24:55 +0200

    New Build

--- obj/x64/Debug/net8.0/win-x64/DelEmptyFolders.AssemblyInfo.cs
+++ obj/x64/Debug/net8.0/win-x64/DelEmptyFolders.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DelEmptyFolders")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.1")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.1+174743d82f70da10fd008459e63101d95f8706c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.1+68b6e93e8d474e0aa4bd06a1cf948229a065477c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DelEmptyFolders")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DelEmptyFolders")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.1")]
---
+++ readme.md
@@ -0,0 +1,11 @@
+# DelEmptyFolders
+
+`DelEmptyFolders` is a simple console application written in C# that recursively deletes empty folders starting from a specified directory. It also provides a \"what-if\" mode to simulate the deletion process without actually deleting any folders.
+## Usage
+
+```sh
+DelEmptyFolders.exe <startingDirectory> [-whatif]
+```
+Examples:
+        DelEmptyFolders.exe C:\path\to\starting\directory
+        DelEmptyFolders.exe C:\path\to\starting\directory -whatif
