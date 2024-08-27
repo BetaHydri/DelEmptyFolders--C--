98891eb9d18b1e42d5e05d9ded3495398c70d9e4
BetaHydri <jan_tiedemann@hotmail.com>
2024-08-27 10:53:25 +0200

    OSX and Linux native builds added

--- obj/x64/Debug/net8.0/win-x64/DelEmptyFolders.AssemblyInfo.cs
+++ obj/x64/Debug/net8.0/win-x64/DelEmptyFolders.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DelEmptyFolders")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.5")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.5+888997d12ea492826ace17905212d6097289feb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.5+412f89f2d6153f451c42d14e9f25fb1bcd305e68")]
 [assembly: System.Reflection.AssemblyProductAttribute("DelEmptyFolders")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DelEmptyFolders")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.5")]
